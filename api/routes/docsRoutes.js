c98eb230e410a12afdea8cd5bfdda30aaa01f9a0
Wendel-007 <tybnkdbj35@gmail.com>
2026-01-15 21:39:10 -0300

    feat(q01): add routes for login and integration pages

--- api/routes/docsRoutes.js
+++ api/routes/docsRoutes.js
@@ -11,4 +11,28 @@ router.get("/docs", (req, res) => {
 	res.sendFile(path.join(__dirname, "..", "public", "docs.html"));
 });
 
+/**
+ * GET /login
+ * Returns login page
+ */
+router.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "..", "web", "login.html"));
+});
+
+/**
+ * GET /integration
+ * Returns API integration page
+ */
+router.get("/integration", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "..", "web", "integration.html"));
+});
+
+/**
+ * GET /
+ * Redirect to login page
+ */
+router.get("/", (req, res) => {
+	res.redirect("/login");
+});
+
 module.exports = router;
